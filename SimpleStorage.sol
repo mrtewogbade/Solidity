2e6af5537c8c020b2df783eb708d1231a3c0d023
mrtewogbade <johndharey@gmail.com>
2023-12-27 16:17:42 +0100

    blockbchain is fun

--- SimpleStorage.sol
+++ SimpleStorage.sol
@@ -1,3 +1,15 @@
 // SPDX-License-Identifier: MIT
-pragma solidity >= 0.8.19; // this is the version
+pragma solidity  0.8.19; // stating our version
 
+
+contract SimpleStorage {
+    // Basic Types: boolean, uint, int, address, bytes
+
+    bool hasFavoriteNumber = false;
+
+    int256 favoriteNumber = -88;
+    string favoriteNumberInText = "88";
+    address myAddress = 0x62a87c1B3ebB21899Cd029337dD3505BE7874449;
+    bytes32 favoriteBytes32 = 'cat';
+
+}
